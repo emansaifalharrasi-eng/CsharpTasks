beb72335d2642e889d09a2df64215a2637a6d7f1
pc1 <pc1>
2026-01-04 12:47:31 +0400

    adding task1

--- Program.cs
+++ Program.cs
@@ -6,16 +6,37 @@ namespace CodeTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first number");
-            float number1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the second number");
-            float number2 = float.Parse(Console.ReadLine());
-            float result = number1 + number2;
-            Console.WriteLine(" the result is " +result);
-            float average = result/ 2;
-            Console.WriteLine("the average is :" +average);
+            while (true)
+            {
+
+                Console.WriteLine("Welcome to system");
+                Console.WriteLine("select the process you need:");
+                Console.WriteLine("1.The avrage of to numbers. ");
+
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                 case 1:
+
+                Console.WriteLine("Enter the first number");
+                float number1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second number");
+                float number2 = float.Parse(Console.ReadLine());
+                float result = number1 + number2;
+                Console.WriteLine(" the result is " + result);
+                float average = result / 2;
+
+                Console.WriteLine("the average is :" + average);
+
+                break;
+            }
+
+               
+
             
             
+            }
         }
     }
 }
+
